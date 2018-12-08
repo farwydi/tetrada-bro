cc50c63ef731335f49ba5240b4c75f8cfd7d3ced
Leonid Zharikov <leon.zharikov@gmail.com>
2018-12-09 01:43:45 +0300

    Поправил вызов функции

--- trans.py
+++ trans.py
@@ -9,9 +9,9 @@ def to_trans(nmap, r='return', i=0, var_i=0):
             for a in lex[1][1]:
                 trans_b, i, var_ix = to_trans(poliz(a), 'push', i, var_i)
                 trans += trans_b
-            trans.append((i, "argx", len(lex[1][1]), "", ""))
-            i += 1
-            trans.append((i, "call", lex[1][0], "", f"${var_i}"))
+            # trans.append((i, "argx", len(lex[1][1]), "", ""))
+            # i += 1
+            trans.append((i, "call", lex[1][0], len(lex[1][1]), f"${var_i}"))
             stack.append((-1, f"${var_i}"))
             var_i += 1
         elif lex[0] in [1, 0]:
